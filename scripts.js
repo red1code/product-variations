044701acc086855af0a2b3e2516363ff7823c6e3
Redouane Bekkouche <redouane.bekk@gmail.com>
2023-04-28 12:02:55 +0100

    refactoring the code for more automation (implement CSV file input)

--- scripts.js
+++ scripts.js
@@ -1,89 +1,85 @@
-const form = document.querySelector('form');
-const productName = document.getElementById('product-name');
-const tableTitle = document.getElementById('table-title');
-const table = document.getElementById('table');
+const csvFileInput = document.getElementById('csv-file-input');
 
 
-productName.addEventListener('keyup', evt => {
-  tableTitle.textContent = `${productName.value} Attributes`;
+let csvData = [];
+let variationsRowsContainer = [
+  ['ID', 'Type', 'SKU', 'Name', 'Published', 'Is featured?', 'Visibility in catalog', 'Short description', 'Description', 'Date sale price starts', 'Date sale price ends', 'Tax status', 'Tax class', 'In stock?', 'Stock', 'Low stock amount', 'Backorders allowed?', 'Sold individually?', 'Weight (kg)', 'Length (cm)', 'Width (cm)', 'Height (cm)', 'Allow customer reviews?', 'Purchase note', 'Sale price', 'Regular price', 'Categories', 'Tags', 'Shipping class', 'Images', 'Download limit', 'Download expiry days', 'Parent', 'Grouped products', 'Upsells', 'Cross-sells', 'External URL', 'Button text', 'Position', 'Attribute 1 name', 'Attribute 1 value(s)', 'Attribute 1 visible', 'Attribute 1 global']
+];
+
+
+csvFileInput.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  reader.onload = (event) => {
+    const fileData = event.target.result;
+    csvData = readCsvData(fileData);
+    for (let i = 0; i < csvData.length - 1; i++) {
+      const attrs = getAttributesFromRow(csvData[i]);
+      const productVariations = generateVariations(attrs);
+      const csvRows = variationsToRows(productVariations, attrs);
+      csvRows.map(row => {
+        variationsRowsContainer.push(row);
+      })
+    }
+    generateCsv(variationsRowsContainer);
+  };
+  reader.readAsText(file);
 });
 
 
-const App = (() => {
-  let _Attributes = [];
-  const getAttributes = () => _Attributes;
-  const saveNewAttribute = (attrInputName, attrInputValues) => {
-    const newAttribute = {
-      attributeName: getInputValue(attrInputName),
-      attributeValues: strToArray(getInputValue(attrInputValues))
-    };
-    _Attributes.push(newAttribute);
-    renderTable();
-    clearInput('attribute-name');
-    clearInput('attribute-items');
+function readCsvData(csvString) {
+  const lines = csvString.split('\n');
+  const headers = lines[0].split(',');
+  const result = [];
+  for (let i = 1; i < lines.length; i++) {
+    const obj = {};
+    const currentLine = lines[i].split(',');
+    let fieldIndex = 0;
+    for (let j = 0; j < headers.length; j++) {
+      let field = currentLine[fieldIndex];
+      if (field && field.startsWith('"')) {
+        // Concatenate fields until we find a closing quote
+        do {
+          field += `,${currentLine[++fieldIndex]}`;
+        } while (fieldIndex < currentLine.length && !currentLine[fieldIndex].endsWith('"'));
+      }
+      obj[headers[j]] = field;
+      fieldIndex++;
+    }
+    result.push(obj);
   }
-  return { getAttributes, saveNewAttribute }
-})();
-
-
-function renderTable() {
-  table.innerHTML = `
-    <tr>
-      <th>Attributes</th>
-      <th>Attribute Name</th>
-      <th>Attribute Values</th>
-    </tr>`;
-  let i = 1;
-  App.getAttributes().map(attr => {
-    table.innerHTML += `
-      <tr>
-        <th>Attribute ${i}</th>
-        <td>${attr.attributeName}</td>
-        <td>${attr.attributeValues}</td>
-      </tr>`;
-    i++
-  })
+  return result;
 }
 
 
-function saveNewAttribute() {
-  App.saveNewAttribute('attributeName', 'attributeItems')
+function getAttributesFromRow(row) {
+  const attributes = [];
+  for (let i = 1; i < 10; i++) {
+    const attrName = `AttributeName${i}`;
+    const attrVals = `AttributeValues${i}`
+    if (row[attrName]) {
+      const newAttribute = {
+        attributeName: row[attrName],
+        attributeValues: stringToArray(row[attrVals])
+      };
+      attributes.push(newAttribute);
+    }
+    else {
+      break;
+    }
+  }
+  return attributes;
 }
 
 
-function getVariations() {
-  const variations = generateVariations(App.getAttributes());
-  console.warn(variations);
-  generateCsv(variations);
-}
-
-
-function getInputValue(inputName) {
-  return form.elements.namedItem(inputName).value;
-}
-
-
-function clearInput(inputID) {
-  document.getElementById(inputID).value = '';
-}
-
-
-function strToArray(str) {
-  return str.split(",").map(val => val.trim());
-}
-
-
-// Function to generate all possible variations from attributes
 function generateVariations(attributes) {
-  const keys = attributes.map((attr) => attr.attributeName);
-  const values = attributes.map((attr) => attr.attributeValues);
-
+  const keys = attributes.map(attr => attr.attributeName);
+  const values = attributes.map(attr => attr.attributeValues);
   const cartesianProduct = (...arrays) =>
     arrays.reduce((acc, arr) =>
-      acc.flatMap((x) =>
-        arr.map((y) =>
-          [...x, y])), [[]]);
-
+      acc.flatMap(x => arr.map(y => [...x, y])),
+      [[]]
+    );
   const variations = cartesianProduct(...values).map((productVariation) =>
     keys.reduce(
       (acc, key, i) => ({
@@ -93,38 +89,56 @@ function generateVariations(attributes) {
       {}
     )
   );
-
   return variations;
 }
 
 
-function generateCsv(variations) {
-  // Define the data for the CSV file
-  const data = variations.map(variation => {
-    let row = ['', `variation`, ``, `${productName.value}`, '1', `0`, `visible`, '', '', '', '', 'taxable', 'parent', '1', '', '', '0', '0', '', '', '', '', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+function variationsToRows(variations, attributes) {
+  let variationsRows = [];
+  variations.map(variation => {
+    let row = ['', 'variation', '', 'replace with product name', '1', '0', 'visible', '', '', '', '', 'taxable', 'parent', '1', '', '', '0', '0', '', '', '', '', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
     for (const [key, value] of Object.entries(variation)) {
-      row = row.concat([key, value, "", "1"]);
+      row = row.concat([key, value, '', '1']);
     }
-    return row;
+    variationsRows.push(row);
   });
+  variationsRows.unshift(generateParentRow(attributes));
+  return variationsRows;
+}
 
-  const csvHeader = ['ID', 'Type', 'SKU', 'Name', 'Published', 'Is featured?', 'Visibility in catalog', 'Short description', 'Description', 'Date sale price starts', 'Date sale price ends', 'Tax status', 'Tax class', 'In stock?', 'Stock', 'Low stock amount', 'Backorders allowed?', 'Sold individually?', 'Weight (kg)', 'Length (cm)', 'Width (cm)', 'Height (cm)', 'Allow customer reviews?', 'Purchase note', 'Sale price', 'Regular price', 'Categories', 'Tags', 'Shipping class', 'Images', 'Download limit', 'Download expiry days', 'Parent', 'Grouped products', 'Upsells', 'Cross-sells', 'External URL', 'Button text', 'Position', 'Attribute 1 name', 'Attribute 1 value(s)', 'Attribute 1 visible', 'Attribute 1 global'];
 
-  data.unshift(csvHeader);
-
-  // Convert the data to a CSV format
-  const csvData = data.map(row => row.join(',')).join('\n');
-
+function generateCsv(csvRows) {
+  const csvData = csvRows.map(row => row.join(',')).join('\n');
   // Create a Blob object with the CSV MIME type
   const blob = new Blob([csvData], { type: 'text/csv' });
-
   // Create a download link and trigger the download
   const url = URL.createObjectURL(blob);
   const link = document.createElement('a');
   link.href = url;
-  link.download = productName.value || 'products.csv';
+  link.download = 'products-variations.csv';
   link.click();
-
   // Clean up the URL object
   URL.revokeObjectURL(url);
+}
+
+
+/*
+helper functions
+*/
+
+
+function generateParentRow(attributes) {
+  const parentRow = ['replace with ID', 'variation', '', 'replace with product name', '1', '0', 'visible', 'replace with short description', '', '', '', 'taxable', '', '1', '', '', '0', '0', '', '', '', '', '1', '', '', '', 'replace with Category', '', '', 'replace with image', '', '', '', '', '', '', '', '', 'replace with position'];
+  const newCols = attributes.map(attr => [
+    attr.attributeName,
+    `"${attr.attributeValues.toString()}"`,
+    '1',
+    '1'
+  ]);
+  return parentRow.concat(newCols).flat();
+}
+
+
+function stringToArray(str) {
+  return str?.split('-').map(val => val.trim());
 }
